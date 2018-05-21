242f2570d1d2ca5067a76351018ea89b944205bf
dolko <oliver.dolk@live.com.au>
2018-05-21 19:33:29 +1000

    Added ways to make a "note" for the generated website

--- classes/facebook.py
+++ classes/facebook.py
@@ -3,21 +3,29 @@ from flask import Flask, request, send_from_directory, render_template, send_fil
 import requests
 import re
 
+
+#http://localhost:5000/facebook?companyid=wow.ax
 class Facebook(Resource):
     def get(self):
+        company = request.args['companyid'].split('.')[0]
+
+
         url = 'http://188.166.238.3/api/v1/fbstats'
         params = {"startdate": "2018-1-1T0:0:0",
                 "enddate": "2018-5-1T0:0:0",
-                "instid": "wow",
-                "stats": "post_message, post_created_time, post_like_count"
+                "instid": company,
+                "stats": "id, name, website, description ,category, fan_count"
             }
         resp = requests.get(url=url, params=params)
         json = resp.json()
+        final = json['Facebook Statistic Data']
+        '''
         final = []
 
         for post in resp.json()['Facebook Statistic Data']['posts']:
             print(post['post_created_time'])
             if re.search('plastic', (post['post_message'])):
                 final.append(post)
+        '''
 
         return final
